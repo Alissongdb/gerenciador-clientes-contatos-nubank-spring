2c58a0ddc685cf88773900ca67444386aec65a11
Alissongdb <alisson.gabriel5@hotmail.com>
2025-04-16 16:37:10 -0300

    feature/criacao do converter

---
+++ src/main/java/com/desafio/nubank/converter/ClienteConverter.java
@@ -0,0 +1,58 @@
+package com.desafio.nubank.converter;
+
+import com.desafio.nubank.dto.ClienteDTO;
+import com.desafio.nubank.dto.ContatoDTO;
+import com.desafio.nubank.model.Cliente;
+import com.desafio.nubank.model.Contato;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ClienteConverter {
+
+    public Cliente toEntity(ClienteDTO clienteDTO) {
+        return Cliente.builder()
+                .nome(clienteDTO.nome())
+                .email(clienteDTO.email())
+                .cpf(clienteDTO.cpf())
+                .contatos(toContatoEntityList(clienteDTO.contatos()))
+                .build();
+    }
+
+    public ClienteDTO toDto(Cliente cliente) {
+        return ClienteDTO.builder()
+                .nome(cliente.getNome())
+                .email(cliente.getEmail())
+                .cpf(cliente.getCpf())
+                .contatos(toContatoDtoList(cliente.getContatos()))
+                .build();
+    }
+
+    public Contato toContatoEntity(ContatoDTO contatoDTO) {
+        return Contato.builder()
+                .nome(contatoDTO.nome())
+                .email(contatoDTO.email())
+                .telefone(contatoDTO.telefone())
+                .build();
+    }
+
+    public ContatoDTO toContatoDto(Contato contato) {
+        return ContatoDTO.builder()
+                .nome(contato.getNome())
+                .email(contato.getEmail())
+                .telefone(contato.getTelefone())
+                .build();
+    }
+
+    public List<Contato> toContatoEntityList(List<ContatoDTO> dtoList) {
+        return dtoList.stream()
+                .map(this::toContatoEntity)
+                .collect(Collectors.toList());
+    }
+
+    public List<ContatoDTO> toContatoDtoList(List<Contato> entityList) {
+        return entityList.stream()
+                .map(this::toContatoDto)
+                .collect(Collectors.toList());
+    }
+}
